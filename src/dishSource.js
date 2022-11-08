f87ff6dfdeb781ac47fa2eb6d11fd8fdf4506441
Abbas Alubeid <abbas@Abbas-MacBook.local>
2022-11-08 15:57:09 +0100

    Done with TW2.2

--- src/dishSource.js
+++ src/dishSource.js
@@ -1,41 +1,51 @@
-import { BASE_URL, API_KEY } from "../src/apiConfig";
+
+import {
+    BASE_URL,
+    API_KEY
+} from "../src/apiConfig.js"
+
+
 
 function treatHTTPResponseACB(response) {
-  /*TODO throw if the HTTP response is not 200, otherwise return response.json()*/
-    if (response) { return response.json(); }
-    else{throw error;}
+    /*TODO throw if the HTTP response is not 200, otherwise return response.json()*/
+    if (response.status !== 200)
+        throw new Error("HTTP response wrong status: " + response.status);
+    else
+        return response.json();
 }
 
-function getDishDetails() {
-  return fetch(
-    BASE_URL + endpoint + params,
-    {
-      // object literal
-      method: "GET", // HTTP method
-      headers: {
-        // HTTP headers, also object literal
-        "X-Mashape-Key": API_KEY,
-        "x-rapidapi-host":
-          "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-      }, // end of headers object
-    } /* end of second fetch parameter, object */
-  ).then(treatHTTPResponseACB);
+function getDishDetails(dishID) {
+    return fetch(BASE_URL+"recipes/"+dishID+"/information",
+
+     { // object literal
+        "method": "GET", // HTTP method
+        "headers": { // HTTP headers, also object literal
+            'X-Mashape-Key': API_KEY,
+            "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+        } // end of headers object
+    } /* end of second fetch parameter, object */ )
+    .then(treatHTTPResponseACB)
 }
 
-function searchDishes() {
-  return fetch(
-    BASE_URL + endpoint + params,
-    {
-      // object literal
-      method: "GET", // HTTP method
-      headers: {
-        // HTTP headers, also object literal
-        "X-Mashape-Key": API_KEY,
-        "x-rapidapi-host":
-          "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
-      }, // end of headers object
-    } /* end of second fetch parameter, object */
-  ).then(treatHTTPResponseACB);
+function searchDishes(dishObject) {
+
+  const query = new URLSearchParams(dishObject)
+  return fetch(BASE_URL+"recipes/complexSearch?"+query.toString(),
+
+  { // object literal
+     "method": "GET", // HTTP method
+     "headers": { // HTTP headers, also object literal
+         'X-Mashape-Key': API_KEY,
+         "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+     } // end of headers object
+ } /* end of second fetch parameter, object */ )
+ .then(treatHTTPResponseACB)
 }
 
-export { getDishDetails, searchDishes };
+
+
+
+export {
+    getDishDetails,
+    searchDishes
+}
---
+++ src/views/searchFormView.js
@@ -0,0 +1,18 @@
+
+
+function SearchFormView(props){
+        return (
+            <div>
+                <input> </input>
+                <select>
+                    <option>Choose:</option>
+                    <option>{}</option>
+                </select>
+                <button>Search!</button>
+            </div>
+    );
+
+
+    
+}
+
