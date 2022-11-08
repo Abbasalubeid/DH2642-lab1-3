305ad6a813b599c5c80ebfa47ce21cd6032fe57b
Abbas Alubeid <abbas@Abbas-MacBook.local>
2022-11-08 16:45:31 +0100

    final touch

--- src/views/searchFormView.js
+++ src/views/searchFormView.js
@@ -1,18 +1,27 @@
-
-
-function SearchFormView(props){
-        return (
-            <div>
-                <input> </input>
-                <select>
-                    <option>Choose:</option>
-                    <option>{}</option>
-                </select>
-                <button>Search!</button>
-            </div>
+function SearchFormView (props){
+    return (
+        <div>
+            <input  onInput={eventPrinterACB} placeholder="Search..." type="search"></input>
+            <select onInput={eventPrinterACB}>
+                <option>Choose:</option>
+                {props.dishTypeOptions.map(onSelectCB)}
+            </select>
+            <button onClick={searchIsClickedACB}>Search!</button>
+        </div>
     );
 
+    function searchIsClickedACB(e){
+        console.log(e.target.value)
+    }
 
-    
+    function eventPrinterACB(e){
+        console.log(e.target.value)
+    }
+
+        function onSelectCB(String){
+            return <option>{String}</option>
+        }
+
 }
 
+export default SearchFormView;
