406759b5a13dcb2598070a0da4c90e88d6fd5004
Ahmed Bashir <Abibr@kth.se>
2022-11-12 23:12:10 +0100

    searchResultsView.js added

---
+++ src/views/searchResultsView.js
@@ -0,0 +1,26 @@
+function SearchResultsView (props){
+    return (
+        <div>
+            {
+                props.searchResults.map(picturingCB)
+                
+            }
+        </div>
+
+    );
+
+    function picturingCB(dish){
+        return  <span onClick={eventPrinterACB} key={dish.id}>
+                    <img height="100" src={"https://spoonacular.com/recipeImages/" + dish.image}></img>
+                    <div>{dish.title}</div>
+                </span>;
+
+    function eventPrinterACB(e){
+        console.log(e.target.value)
+    }
+
+}
+
+}
+
+export default SearchResultsView;
