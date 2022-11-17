ad5363dc1e71b740e1325b3ce79f1b40ff090546
Abbas Alubeid <abbas@Abbas-MacBook.local>
2022-11-17 22:27:46 +0100

    deleted all files

--- src/reactjs/ReactRoot.js
+++
@@ -1,6 +0,0 @@
-// Add relevant imports here 
-
-// Define the ReactRoot component
-// function ReactRoot()
-
-// Export the ReactRoot component
--- src/vuejs/summaryPresenter.js
+++
@@ -1,8 +0,0 @@
-import { shoppingList } from "../utilities.js";
-import SummaryView from "../views/summaryView.js";
-
-
-export default
-function Summary(props){
-    return <SummaryView people={props.model.numberOfGuests} ingredients={shoppingList(props.model.dishes)}/>;
-}
