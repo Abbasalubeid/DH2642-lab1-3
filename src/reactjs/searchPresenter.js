e54d635c1e39b1a5462effc8b0e9dfd31c17f2b9
Abbas Alubeid <abbas@Abbas-MacBook.local>
2022-11-17 20:47:36 +0100

    Small changes

--- src/reactjs/searchPresenter.js
+++ src/reactjs/searchPresenter.js
@@ -1,28 +1,25 @@
 import React from "react";
-import {render} from "react-dom";
 import SearchFormView from "../views/searchFormView.js"
 
 export default
 function SearchPresenter(props){
 
-    if(!props.model.searchResultsPromiseState.promise){
-        props.model.doSearch(props.model.searchParams); 
-    }
-
     const [searchQuery, setQuery]= React.useState(); 
     const [searchType, setSearchType]= React.useState();
-    
-    // const [searchParam, setSearchParam] = React.useState({query: " " , type : " "});
-
+    const [searchParams, setSearchParams] = React.useState({});
     
 
-    
-    function handleInputACB1(string){ setQuery(string); }
+    // if(!props.model.searchResultsPromiseState.promise){
+    //     props.model.doSearch(props.model.searchParams); 
+    // }
 
-    function handleInputACB2(string){ setSearchType(string); }
+    function handleInputACB1(string){ searchParams.query = string; }
 
-    function handleInputACB3(){ 
-        props.model.doSearch(searchParam);
+    function handleInputACB2(string){ searchParams.type = string;  }
+
+    function handleInputACB3(){
+
+        props.model.doSearch(searchParams)
 }
 
 
@@ -33,7 +30,7 @@ function userChoseACB(string){props.model.setSearchType(string)}
 function userSearchedACB(){props.model.doSearch(props.model.searchParams)} 
 
 return <div>
-        <SearchFormView   dishTypeOptions={["Starter", "Main course", "Dessert"]}
+        <SearchFormView   dishTypeOptions={['starter', 'main course', 'dessert']}
                           onUserTyped={handleInputACB1}
                           onUserChose={handleInputACB2} 
                           onUserSearched={handleInputACB3}    />
