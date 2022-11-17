faa62d15a4ae7203585229712caf057b321566af
Abbas Alubeid <abbas@Abbas-MacBook.local>
2022-11-17 22:16:28 +0100

    done with 3.2

--- src/reactjs/searchPresenter.js
+++ src/reactjs/searchPresenter.js
@@ -1,39 +1,75 @@
 import React from "react";
-import SearchFormView from "../views/searchFormView.js"
+import SearchFormView from "../views/searchFormView.js";
+import SearchResultsView from "../views/searchResultsView"
+import resolvePromise1 from "../resolvePromise.js";
+import { searchDishes } from "../dishSource.js";
+import promiseNoData from "../views/promiseNoData.js"
 
-export default
-function SearchPresenter(props){
+export default function SearchPresenter(props) {
+  // const [searchQuery, setQuery]= React.useState();
+  // const [searchType, setSearchType]= React.useState();
+  const [searchParams, setSearchParams] = React.useState({});
+  const [promiseState] = React.useState({});
+  const [, reRender] = React.useState();
 
-    const [searchQuery, setQuery]= React.useState(); 
-    const [searchType, setSearchType]= React.useState();
-    const [searchParams, setSearchParams] = React.useState({});
-    
+  if (!promiseState.promise) {
+    resolvePromise1(searchDishes(searchParams), promiseState, notifyACB);
+  }
 
-    // if(!props.model.searchResultsPromiseState.promise){
-    //     props.model.doSearch(props.model.searchParams); 
-    // }
+  function notifyACB() {
+    const newProm = {};
+    reRender(newProm);
+  }
 
-    function handleInputACB1(string){ searchParams.query = string; }
+  function handleInputACB1(string) {
+    const obj = {};
+    obj.query = string;
+    obj.type = searchParams.type;
+    setSearchParams(obj);
+  }
 
-    function handleInputACB2(string){ searchParams.type = string;  }
+  function handleInputACB2(string) {
+    const obj = {};
+    obj.type = string;
+    obj.query = searchParams.query;
+    setSearchParams(obj);
+  }
 
-    function handleInputACB3(){
+  function handleInputACB3() {
+    resolvePromise1(searchDishes(searchParams), promiseState, notifyACB);
+  }
 
-        props.model.doSearch(searchParams)
-}
+  function handleInputACB4(dish) {
+    props.model.setCurrentDish(dish.id)
+  }
 
+  function userTypedACB(string) {
+    props.model.setSearchQuery(string);
+  }
 
-function userTypedACB(string){props.model.setSearchQuery(string)} 
+  function userChoseACB(string) {
+    props.model.setSearchType(string);
+  }
 
-function userChoseACB(string){props.model.setSearchType(string)} 
+  function userSearchedACB() {
+    props.model.doSearch(props.model.searchParams);
+  }
 
-function userSearchedACB(){props.model.doSearch(props.model.searchParams)} 
-
-return <div>
-        <SearchFormView   dishTypeOptions={['starter', 'main course', 'dessert']}
-                          onUserTyped={handleInputACB1}
-                          onUserChose={handleInputACB2} 
-                          onUserSearched={handleInputACB3}    />
-                          </div>;
-
+  function userChoseDishACB(dish){props.model.setCurrentDish(dish.id)}
+return (
+    <div>
+      <SearchFormView
+        dishTypeOptions={["starter", "main course", "dessert"]}
+        onUserTyped={handleInputACB1}
+        onUserChose={handleInputACB2}
+        onUserSearched={handleInputACB3}
+      />
+      {promiseNoData(promiseState) || 
+        <SearchResultsView
+          searchResults={promiseState.data}
+          onUserChoseDish={handleInputACB4}
+        />
+      }
+    </div>
+  );
 }
